15dabd94520a256fbb64a5fce70ab9b044b6bc99
AfterRain <lihaoyuforwork@163.com>
2022-07-27 18:12:11 +0800

    navbar

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <navbar />
+  <router-view></router-view>
+</template>
+
+<script setup>
+import navbar from './components/navbar/index.vue'
+</script>
+
+<style>
+</style>
---
+++ src/components/navbar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar">
+    <div class="link-container">
+      <router-link>
+        <span>About me</span>
+      </router-link>
+      <router-link>
+        <span>About The Website</span>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="less" scoped>
+.navbar {
+  height: 50px;
+  width: 100vw;
+  line-height: 50px;
+  background-color: #ccc;
+}
+.link-container {
+  float: right;
+  span {
+    display: inline-block;
+    background-color: #fff;
+    margin-right: 25px;
+    transition: all 0.5s;
+    cursor: pointer;
+  }
+  span:hover {
+    background-color: aqua;
+  }
+}
+</style>
