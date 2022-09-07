d61813c4db381ca1ca74758b5ee82b0b19799fc5
李昊雨 <lihaoyu@lihaoyudeMacBook-Air.local>
2022-09-07 20:26:31 +0800

    编译

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router";
-import "animate.css";
-
-const app = createApp(App);
-app.use(router).mount("#app");
--- src/pages/home/note.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="note"><h2>| 个人简历</h2></div>
-</template>
-
-<script setup></script>
-
-<style lang="less" scoped></style>
